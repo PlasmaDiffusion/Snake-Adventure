338c11f95ee146864ebe6e82e3b704bfa6cbc1ed
PlasmaDiffusion <Scott50000@gmail.com>
2019-06-19 17:36:07 -0400

    UV Fix
    [ADD] -UV tiling script to make the stretching less uglu
    
    [MOD] -Snake cross eyes

---
+++ Assets/Scripts/LevelGeneration/RetileUVs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetileUVs : MonoBehaviour
+{
+    public Vector2 newUVScale;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTextureScale = newUVScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
